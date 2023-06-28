baca2b2db364821e6b1af58c44c535255ea11747
ya121 <ya7771@yandex.ru>
2023-06-28 16:55:11 +0300

    Сделано ДЗ № 3_14 Логирование и конфигурирование

--- src/main/java/ru/skypro/homework/loggerprofiles/controller/InfoController.java
+++ src/main/java/ru/skypro/homework/loggerprofiles/controller/InfoController.java
@@ -2,10 +2,8 @@ package ru.skypro.homework.loggerprofiles.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skypro.homework.loggerprofiles.pojo.AppInfo;
-import ru.skypro.homework.loggerprofiles.pojo.Employee;
 import ru.skypro.homework.loggerprofiles.service.AppService;
 
 @RestController
