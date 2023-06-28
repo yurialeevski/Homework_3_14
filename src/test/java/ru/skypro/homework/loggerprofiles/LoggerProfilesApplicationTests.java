d96bd41fb17e85a7cc4a0baabd9781c921f082c7
ya121 <ya7771@yandex.ru>
2023-06-28 16:52:45 +0300

    Сделано ДЗ № 3_14 Логирование и конфигурирование

---
+++ src/test/java/ru/skypro/homework/loggerprofiles/LoggerProfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.loggerprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggerProfilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
